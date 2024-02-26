8a155d5a4300fa520e21804e88f527fbe9e1ae7c
Daniel Berthran <berthlearn@gmail.com>
2024-02-26 11:18:58 +0100

    create file to validate if commands exist

---
+++ execve_for_task3.c
@@ -0,0 +1,112 @@
+#include "shell.h"
+
+/**
+ * validateCmd - checks if a command exists
+ * @cmd: the command to validate
+ *
+ * Return: 1 if cmd exits, 0 if it doesn't exist
+ */
+
+char *validateCmd(char *cmd)
+{
+	char *path_val;
+	pdir_t *dirList, *tempDir;
+	struct stat st;
+	char *filePath;
+
+	/* Access the PATH variable */
+	path_val = getPATH();
+	/* Create a linked list of the directories in PATH */
+	dirList = makePathList(path_val);
+
+	/* Handle different command format */
+	switch (*cmd)
+	{
+		/* Absolute Path */
+		case '/':
+		/* Relative Path */
+		case '.':
+			free_pdir(dirList);
+			if (lstat(cmd, &st) == 0)
+				return (cmd);
+			else
+				return (NULL);
+		default:
+			while (dirList)
+			{
+				filePath = absPath(dirList->dir, cmd);
+				printf("filepath %s\n", filePath);
+				if (lstat(filePath, &st) == 0)
+				{
+
+					free_pdir(dirList);
+					return (filePath);
+				}
+				tempDir = dirList;
+				dirList = dirList->next_dir;
+				free(tempDir);
+				free(filePath);
+			}
+	}
+	free_pdir(dirList);
+	return (NULL);
+}
+
+/**
+ * absPath - creates an absolute path for a command
+ * @dir: the absolute path of a directory
+ * @fileName: the command/file to create an absolute for
+ *
+ * Return: an absolute path with the file name
+ */
+
+char *absPath(char *dir, char *fileName)
+{
+	char *fullPath;
+	size_t dirLen;
+	size_t fileLen;
+	size_t fullLen;
+	size_t i, j;
+
+	if (dir == NULL)
+		return (NULL);
+
+	dirLen = strlen(dir);
+	fileLen = strlen(fileName);
+
+	/* Dir with and without '/' ending */
+	if (dir[dirLen - 1] != '/')
+		fullLen = dirLen + 1 + fileLen + 1;
+	else
+		fullLen = dirLen + fileLen + 1;
+
+	/* abs path looks like -> */
+	/* "/usr/bin" + "/" + "ls" */
+	fullPath = (char *)malloc(sizeof(char) * fullLen);
+
+	/* Handling malloc fail */
+	if (fullPath == NULL)
+		return (NULL);
+
+	/* Writing directory */
+	for (i = 0; i < dirLen; i++)
+		fullPath[i] = dir[i];
+
+	/* Writing '/' */
+	if (dir[i - 1] != '/')
+	{
+		fullPath[i] = '/';;
+		++i;
+	}
+
+	/* Writing filename */
+	for (j = 0; j < fileLen; j++)
+		fullPath[j + i] = fileName[j];
+	
+	fullPath[i + j] = '\0';
+
+	return (fullPath);
+}
+
+
+
