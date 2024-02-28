563d0cabe8d247715472076bf2688af2d2a541c7
Daniel Berthran <berthlearn@gmail.com>
2024-02-28 11:53:35 +0100

    update header files

---
+++ shell.h
@@ -0,0 +1,67 @@
+#ifndef SHELL_H
+#define SHELL_H
+#define _GNU_SOURCE
+
+/* Header Files */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+/**
+ * struct pathdir - a data type for storing directories in the PATH variable
+ * @dir: the string name of the directory
+ * @next_dir: pointer to the next directory
+ */
+
+typedef struct pathdir
+{
+	char *dir;
+	struct pathdir *next_dir;
+} pdir_t;
+
+
+/* Function Prototypes */
+
+char *getPATH(void);
+
+pdir_t *makePathList(char *pathVal);
+
+int _INT_MODE(char **argv, pdir_t **dirHead);
+
+int _NON_INT_MODE(char **argv, pdir_t **dirHead);
+
+char *promptline(char *line);
+
+int isNewline(char *line);
+
+int isShellBuiltin(char **cmdline, pdir_t **dirHead);
+
+int is_env(char *cmdline);
+
+int is_exit(char **cmdline, pdir_t **dirHead);
+
+int processCmds(char *cmdline, char **argv, pdir_t **dirHead);
+
+char **createArgsArr(char *cmdline);
+
+char *validateCmd(char *cmd, pdir_t **dirHead);
+
+int executeCmds(char *cmdline, char *cmd, char **arr, pdir_t **dirHead);
+
+pdir_t *add_dir(pdir_t **path_head, char *dir);
+
+pdir_t *add_dir_end(pdir_t **path_head, char *dir);
+
+int free_pdir(pdir_t *path_head);
+
+char *absPath(char *dir, char *fileName);
+
+
+
+int printenv(char *cmdline);
+
+#endif /* SHELL_H */
