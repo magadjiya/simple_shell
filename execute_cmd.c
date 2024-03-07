5d8e84c48ce7fd1a6c2495c3b1484dbe7c3f575f
Daniel Berthran <berthlearn@gmail.com>
2024-03-07 12:25:38 +0100

    update file

--- execute_cmd.c
+++ execute_cmd.c
@@ -4,7 +4,6 @@
  * executeCmds - executes a command given
  * @cmdline: the command line to be freed if fails
  * @cmd: the line of command to execute
- * @fline: a commmand line to free
  * @arr: array of arguments to execute
  * @dirHead: pointer to linked list of directories in PATH
  * @aliasHead: pointer to the linked list of aliases
@@ -14,8 +13,8 @@
  * Return: 0 on success, -1 on failure
  */
 
-int executeCmds(char *cmdline, char *cmd, char *fline, char **arr,
-		pdir_t **dirHead, alias **aliasHead, char *envp[], char *pathValcpy)
+int executeCmds(char *cmdline, char *cmd, char **arr, pdir_t **dirHead,
+	alias **aliasHead, char *envp[], char *pathValcpy)
 {
 	int wstatus;
 	pid_t p;
@@ -34,10 +33,8 @@ int executeCmds(char *cmdline, char *cmd, char *fline, char **arr,
 			/* free up allocated memory spaces in child process */
 			if (arr[0] != cmd)
 				free(cmd);
-			free(arr[0]);
-			free(arr);
+			free_arr(arr);
 			free(cmdline);
-			free(fline);
 			free_pdir(*dirHead);
 			free_alias(*aliasHead);
 			free(pathValcpy);
