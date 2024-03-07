87e237de3c4510b820a67ee5f504a2271dc90397
Daniel Berthran <berthlearn@gmail.com>
2024-03-07 12:26:09 +0100

    modify process_cmds to be a standalone function for processing commands

--- process_cmds.c
+++ process_cmds.c
@@ -2,77 +2,44 @@
 
 /**
  * processCmds - handles the processing of commands passed to the terminal
- * @cmdline: the line of command to process
- * @argv: array of arguments to shell program
- * @dirHead: the head of the path directory linked list
+ * @arr: an array containing the commands to process
+ * @cmdline: the command line to be freed if fails
+ * @argv: array of arguments given to the shell program
+ * @dirHead: pointer to the path directory linked list
  * @envp: the array of environment variables
  * @aliasHead: pointer to the linked list of aliases
- * @pathValcpy: copy of directory string in PATH
+ * @pathValcpy: copy of the value string of 'PATH'
  *
  * Return: 0 on success, -1 on failure
  */
 
-int processCmds(char *cmdline, char **argv, pdir_t **dirHead,
+int processCmds(char **arr, char *cmdline, char **argv, pdir_t **dirHead,
 		char *envp[], alias **aliasHead, char *pathValcpy)
 {
-	char *line, *fullCmd, *new_line;
-	char **arr = NULL;
+	char *fullCmd;
 	int status;
-
-	/* Make a copy of the command line */
-	line = strdup(cmdline);
-	new_line = noWhiteSpaces(line);
-
-	/* Create array of arguments */
-	arr = createArgsArr(line);
 
 	if (arr == NULL)
 		return (0);
 
-	/*  Validate the command @ arr[0] */
+	/*  Validate the command @ arr[0] or get its fullpath */
 	fullCmd = validateCmd(&arr[0], dirHead, aliasHead);
 
 	/* Invalid command */
 	if (fullCmd == NULL)
 	{
 		fprintf(stderr, "%s: 1: %s: not found\n", argv[0], arr[0]);
-		/*print_error_message(argv, arr);*/
-		free(new_line);
-		free(arr[0]);
-		free(arr);
+		free_arr(arr);
 		return (127);
 	}
+
 	/* Valid command */
 	else
-		status = executeCmds(cmdline, fullCmd, new_line,
-		arr, dirHead, aliasHead, envp, pathValcpy);
+		status = executeCmds(cmdline, fullCmd, arr, dirHead,
+				aliasHead, envp, pathValcpy);
 
-	if (*new_line != '/' && *new_line != '.')
+	if (arr[0] != fullCmd)
 		free(fullCmd);
-	free(new_line);
-	free(line);
-	free(arr);
+	free_arr(arr);
 	return (status);
-}
-
-/**
- * print_error_message - creates and prints an error message
- * if command is invalid
- * @argv: array of arguments containing name of program
- * @arr: array of cmdlind arguments containing name of command
- *
- * Return: pointer to error message
- */
-
-int print_error_message(char **argv, char **arr)
-{
-	char errmsg[100];
-
-	strcpy(errmsg, argv[0]);
-	strcat(errmsg, ": 1: ");
-	strcat(errmsg, arr[0]);
-	strcat(errmsg, ": not found\n");
-
-	/*write(STDOUT_FILENO, errmsg, strlen(errmsg));*/
-	return (0);
 }
